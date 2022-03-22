6e34fca793f5453876014663d94da4c1ffcaaf16
sianwa11 <33891397+sianwa11@users.noreply.github.com>
2022-03-22 09:22:27 +0300

    Added cart items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import CartItems from "./components/Cart/CartItems";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <CartItems />
     </>
   );
 }
--- src/components/Cart/CartItems.js
+++ src/components/Cart/CartItems.js
@@ -1,14 +1,30 @@
 import React from "react";
+import styles from "./CartItem.module.scss";
 
 const CartItems = () => {
   return (
     <>
-      <div>
-        <div className="">Item Name</div>
-        <div className="">Item Price</div>
-        <div className="">Item Quantity</div>
-        <div className="buttons">+</div>
-        <div className="buttons">-</div>
+      <div className={styles.items}>
+        <div className={styles.item}>
+          <div className={styles.item__details}>
+            <div className={styles["item__details--price"]}>$86</div>
+            <div className={styles["item__details--name"]}>
+              Item NameIntroduction to Algorithms, 3rd Edition
+              <span>by Thomas H. Cormen</span>
+            </div>
+          </div>
+          <div className={styles.item__actions}>
+            <div className="item__actions--1">
+              <div className="buttons">+</div>
+              <div className="">Item Quantity</div>
+              <div className="buttons">-</div>
+            </div>
+
+            <div className="item__actions--2">
+              <div className="buttons">Delete</div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
